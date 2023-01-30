7b6b12535b71b9a465855ec2641eea0c013118fa
Marco Rodrigues <marcoaurelio853@gmail.com>
2023-01-30 19:17:38 +0000

    add  HandlerFactory

--- src/controllers/HandlerFactory.ts
+++ src/controllers/HandlerFactory.ts
@@ -1,14 +1,18 @@
 import express from 'express';
 
-export const createOne = (Model) => {
-  async (req: express.Request, res: express.Response) => {
-    const response = await Model.create(req.body);
+class HandlerFactory {
+  createOne = (Model: any) => {
+    async (req: express.Request, res: express.Response) => {
+      const response = await Model.create(req.body);
 
-    res.status(201).json({
-      status: 'success',
-      data: {
-        data: response,
-      },
-    });
+      res.status(201).json({
+        status: 'success',
+        data: {
+          data: response,
+        },
+      });
+    };
   };
-};
+}
+
+export default new HandlerFactory();
