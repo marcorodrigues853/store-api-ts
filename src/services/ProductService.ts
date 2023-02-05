cd115c075a6791340d59a2da67ad4a82e5f6010b
Marco Rodrigues <marcoaurelio853@gmail.com>
2023-02-05 22:20:25 +0000

    Add  APIFilters Class and use it

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,16 +1,25 @@
 import { Product } from '../models/ProductsModel';
 import { IProduct } from './../interface/IProducts';
+import { Request } from 'express';
 import AppError from '../utilities/AppError';
 
-import { createOne } from './../controllers/HandlerFactory';
+import APIFilters from '../utilities/ApiFilters';
 class ProductService {
   async create(product: IProduct) {
     const newProduct = await Product.create(product);
     return newProduct;
   }
-  async getAll() {
-    const allProducts = await Product.find();
-    return allProducts;
+  async getAll(queryObject: any) {
+    const filters = new APIFilters(Product.find(), queryObject)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
+    console.log('filters: ', filters);
+
+    const products: IProduct[] = await filters.query;
+    return products;
   }
   async getOne(id: string) {
     if (!id) new AppError('Please pass an  product id.', 422);
@@ -18,10 +27,12 @@ class ProductService {
     const product = await Product.findById(id);
     return product;
   }
-  async delete() {}
-  async update() {}
+  async update() {
+    //
+  }
 }
 // const createProductAlex = factory.createOne(ProductAlex);
+// const createUserAlex = factory.createOne(UsertAlex);
 // const getProductAlex = factory.getOne(ProductAlex);
 // const getAllProductAlex = factory.getAll(ProductAlex);
 // const updateProductAlex = factory.updateOne(ProductAlex);
---
+++ src/utilities/APIFilters.ts
@@ -0,0 +1,80 @@
+import express from 'express';
+
+class APIFilters {
+  query;
+  queryString;
+
+  /**
+   *
+   * @param query  coming from mongoose
+   * @param queryString coming from express route
+   */
+
+  constructor(query: any, queryString: any) {
+    this.query = query; // query of argument
+    this.queryString = queryString;
+  }
+
+  /**
+   *
+   * @returns
+   */
+  filter() {
+    const queryObject = { ...this.queryString };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObject[el]);
+
+    let queryStr = JSON.stringify(queryObject);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
+
+    return this; // to use chain method and next method has access
+  }
+
+  /**
+   *
+   * @returns
+   */
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort('-createdAt');
+    }
+
+    return this;
+  }
+
+  /**
+   *
+   * @returns
+   */
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select('-__v');
+    }
+
+    return this;
+  }
+
+  /**
+   *
+   * @returns
+   */
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
+}
+
+export default APIFilters;
