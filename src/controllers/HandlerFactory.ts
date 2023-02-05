d8f8929cf11c2bf0af2a64e9dfb13ac6c6d0abee
Marco Rodrigues <marcoaurelio853@gmail.com>
2023-02-05 22:18:07 +0000

    fix imports

--- src/controllers/HandlerFactory.ts
+++ src/controllers/HandlerFactory.ts
@@ -1,7 +1,8 @@
-import express from 'express';
+import { mongoose, Model } from 'mongoose';
+import { Request, Response } from 'express';
 
-export const createOne = (Model: any) => {
-  async (req: express.Request, res: express.Response) => {
+export const createOne = (Model: Model<unknown>) => {
+  async (req: Request, res: Response) => {
     const response = await Model.create(req.body);
 
     res.status(201).json({
