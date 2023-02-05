d4194ebee096d2982cb25c2419fc3adcb61479cc
Marco Rodrigues <marcoaurelio853@gmail.com>
2023-02-05 22:20:59 +0000

    add tou testing

--- src/controllers/tourController.ts
+++ src/controllers/tourController.ts
@@ -1,26 +1,21 @@
-wimport express from 'express';
+import express from 'express';
 
 import TourService from '../services/TourService';
 
-
 class TourController {
-
-
   getAllTours(req: express.Request, res: express.Response) {
     res.status(200).json({
       status: 'success',
-      requestAt: Date.now().toLocaleString()
-    })
+      requestAt: Date.now().toLocaleString(),
+    });
   }
-
 
   getTour(req: express.Request, res: express.Response) {
     //
   }
 
-
   async create(req: express.Request, res: express.Response) {
-    const newTour = await TourService.create(req.body)
+    const newTour = await TourService.create(req.body);
     res.json(newTour);
   }
 
@@ -28,9 +23,7 @@ class TourController {
     //
   }
 
-
-  updateTour(req: express.Request, res: express.Response) { }
+  updateTour(req: express.Request, res: express.Response) {}
 }
-
 
 export default new TourController();
