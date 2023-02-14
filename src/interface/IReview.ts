5ed2c0da032e8ffefaa9c6ad324db2d09cbcaedb
Marco Rodrigues <marcoaurelio853@gmail.com>
2023-02-14 19:29:06 +0000

    add typescript to model

--- src/interface/IReview.ts
+++ src/interface/IReview.ts
@@ -1,6 +1,12 @@
+import { Model } from 'mongoose';
+
 export interface IReview {
   review: string;
   rating: number;
   product: string[];
   user: string[];
+}
+
+export interface IReviewModel extends Model<IReview> {
+  calcAverageRatings: (id: any) => Promise<void>;
 }
