965d5544f4c17ccbd11b4cab4fe124a2e270572f
Marco Rodrigues <marcoaurelio853@gmail.com>
2023-02-14 19:29:00 +0000

    add typescript to model

--- src/controllers/ReviewController.ts
+++ src/controllers/ReviewController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import ReviewService from '../services/ReviewService';
 
 interface RequestWithUser extends Request {
@@ -22,26 +22,30 @@ class Review {
         },
       });
     } catch (error) {
-      res.status(422).json({
-        status: 'error',
-        error,
-      });
+      if (error instanceof Error) {
+        res.status(422).json({
+          status: 'error',
+          error: error.message,
+        });
+      }
     }
   }
 
-  async createOne(req: any, res: Response) {
+  async createOne(req: any, res: Response, next: NextFunction) {
     try {
       if (!req.body.product) req.body.product = req.params.id;
-      console.log('REQ:', req); // TODO: need to be reviewed with Alex Ageev
+      console.log('REQ:', req.body); // TODO: need to be reviewed with Alex Ageev
       if (!req.body.user) req.body.user = req.user.id;
 
       const newReview = await ReviewService.create(req.body);
+      console.log('newReview:', newReview);
       res.status(200).json({
         status: 'success',
         data: {
           review: newReview,
         },
       });
+      next();
     } catch (error) {
       res.status(422).json({
         status: 'error',
