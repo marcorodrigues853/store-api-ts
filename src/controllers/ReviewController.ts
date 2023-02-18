b54b5b8dc3198c70a9a2a2571df25e661e304d7c
Marco Rodrigues <marcoaurelio853@gmail.com>
2023-02-18 22:09:16 +0000

    add delete and update reviews

--- src/controllers/ReviewController.ts
+++ src/controllers/ReviewController.ts
@@ -1,18 +1,49 @@
 import { Request, Response, NextFunction } from 'express';
 import ReviewService from '../services/ReviewService';
+import AppError from '../utilities/AppError';
 
 interface RequestWithUser extends Request {
   user: any;
 }
 
 class Review {
+  async deleteOne(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      if (!id) new AppError('ID not provided', 404);
+
+      const deleted = await ReviewService.deleteOne(id);
+      if (!deleted) new AppError(`Cant't delete`, 200);
+
+      return res.status(200).json(deleted);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        res.status(400).json(error.message);
+      }
+    }
+  }
+
+  async updateOne(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const product = await ReviewService.update(id, req.body);
+      res.status(201).json({ status: 'success', data: product });
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        res.status(500).json({
+          message: 'Failed to update review.',
+          error,
+        });
+      }
+    }
+  }
+
   async getAll(req: Request, res: Response) {
     try {
       let filter = {};
 
       if (req.params.id) filter = { product: req.params.id };
       const reviews = await ReviewService.getAll(filter);
-      // pass to service
 
       res.status(200).json({
         status: 'success',
